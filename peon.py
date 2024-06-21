33b7dccc7e2a3b92115b078bf6397cb701295678
CrysAparic <sarahi_aparicio@siman.com>
2024-06-20 19:58:44 -0600

    se agrega el proyecto

---
+++ peon.py
@@ -0,0 +1,6 @@
+from pieza import *
+
+class Peon(Pieza):
+    def __init__(self, color, nombre):
+        super().__init__(color, nombre)
+        
---
+++ pieza.py
@@ -0,0 +1,4 @@
+class Pieza:
+  def __init__(self, color, nombre):
+    self.color = color
+    self.nombre = nombre
