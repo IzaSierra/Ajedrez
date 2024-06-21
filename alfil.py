48d2ccd1138a7a95dfedd1232f4800dfe35d2b72
IzaSierra <sierrayarely88@gmail.com>
2024-06-20 20:13:02 -0600

    agregando mas

---
+++ alfil.py
@@ -0,0 +1,5 @@
+from pieza import *
+
+class alfil(Pieza):
+    def __init__(self, color, nombre):
+        super().__init__(color, nombre)
---
+++ torre.py
@@ -0,0 +1,5 @@
+from pieza import *
+
+class Torre(Pieza):
+    def __init__(self, color, nombre):
+        super().__init__(color, nombre)
