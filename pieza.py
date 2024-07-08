639a916bf5b72b180446b319587a0ce442bfa869
IzaSierra <sierrayarely88@gmail.com>
2024-07-07 20:18:21 -0600

    se agrego mas

--- pieza.py
+++ pieza.py
@@ -1,4 +1,18 @@
 class Pieza:
-  def __init__(self, color, nombre):
+  def __init__(self, color, nombre, fila, columna):
     self.color = color
     self.nombre = nombre
+    self.fila = fila
+    self.columna = columna
+    
+    def movimiento(self, nueva_posicion):
+        pass
+      
+    def mover(self, nueva_posicion):
+        if self.movimiento(nueva_posicion):
+            self.posicion = nueva_posicion
+            return f"La pieza ahora está en la posición: {nueva_posicion}"
+        else:
+            return "Movimiento Incorrecto, la pieza no se ha movido"
+          
+    
--- torre.py
+++ torre.py
@@ -1,5 +1,10 @@
 from pieza import *
 
 class Torre(Pieza):
-    def __init__(self, color, nombre):
-        super().__init__(color, nombre)
+    def __init__(self, color, nombre, fila, columna):
+        super().__init__(color, nombre, fila, columna)
+
+def movimiento (self,fila, columna):
+    if self.columna == columna:
+        if self.fila == 2:
+            pass    
