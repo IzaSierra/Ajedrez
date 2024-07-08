9bbccf41e6defad8a62a1f420480bb8ace8af644
IzaSierra <sierrayarely88@gmail.com>
2024-07-07 20:14:49 -0600

    añadi metodo movimiento

---
+++ main.py
@@ -0,0 +1,7 @@
+from pieza import *
+from peon import *
+from torre import *
+
+peon1 = Peon("blanco","peon1",2,2)
+
+peon1.movimiento(2,2)
--- peon.py
+++ peon.py
@@ -1,6 +1,12 @@
 from pieza import *
 
 class Peon(Pieza):
-    def __init__(self, color, nombre):
-        super().__init__(color, nombre)
+    def __init__(self, color, nombre,  fila, columna):
+        super().__init__(color, nombre, fila, columna)
+
+
+def movimiento (self,fila, columna):
+    if self.columna == columna:
+        if self.fila == 2:
+            
         
